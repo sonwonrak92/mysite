ed5bba0d8026c3ce31383d285aa55ab41563438c
sonwonrak92 <sonwonrak@gmail.com>
2018-01-08 19:08:26 +0900

    여론조사 사이트추가

--- elections/admin.py
+++ elections/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Candidate
+from .models import Candidate, Poll, Choice
 
 admin.site.register(Candidate)
+admin.site.register(Poll)
+admin.site.register(Choice)
