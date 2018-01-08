0961ce2ce60ef90ecd640b9a6564edfb056ccc55
sonwonrak92 <sonwonrak@gmail.com>
2018-01-08 17:51:19 +0900

    template 사용

--- elections/views.py
+++ elections/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Candidate
+
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse("hello world")
+    candidates = Candidate.objects.all()
+    context = {'candidates': candidates}  # context에 모든 후보에 대한 정보를 저장
+    # context로 html에 모든 후보에 대한 정보를 전달
+    return render(request, 'elections/index.html', context)
